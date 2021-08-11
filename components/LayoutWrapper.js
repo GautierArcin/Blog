37b048c9d477c8e3ac2855926286b8adc16b5fa2
Gautier Arcin <gauta82@gmail.com>
2021-08-11 09:26:08 +0200

    feat: responsive design

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -42,8 +42,8 @@ const LayoutWrapper = ({ children }) => {
                     }
                   />
                 </div>
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                {typeof siteMetadata.headerTitle[locale] === 'string' ? (
+                  <div className="h-6 text-2xl invisible xl:visible xl: font-semibold sm:block">
                     {siteMetadata.headerTitle[locale]}
                   </div>
                 ) : (
