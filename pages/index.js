b4055d7d1b85b97e119a998972745ac27039c1b9
Gautier Arcin <gauta82@gmail.com>
2021-09-30 16:38:47 +0200

    wip homepage

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 import useTranslation from 'next-translate/useTranslation'
+import Trans from 'next-translate/Trans'
 
 const MAX_DISPLAY = 5
 
@@ -26,12 +27,19 @@ export default function Home({ posts, locale, availableLocales }) {
         availableLocales={availableLocales}
       />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+        <div className="pt-6 pb-4 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            {t('common:greeting')}
+            {t('home:greetings')}
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description[locale]}
+            {t('home:subtitle1')} <br />
+            <Trans
+              i18nKey="home:subtitle2"
+              components={{
+                link1: <Link className="font-extrabold" href="/about" />,
+                link2: <Link className="font-extrabold" href="/projects" />,
+              }}
+            />
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
